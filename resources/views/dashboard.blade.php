7c2492dbc3f0fcaab16d1da6c3af030abf12a5a2
rupeshthapa <leleventuresadv@gmail.com>
2025-04-25 17:08:10 +0545

    migration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     {{ Auth::user()->name }}
+    <p class="lead">If you want to add products: <a href="#">Add Products</a></p>
+    <p class="lead">If you want to add categories: <a href="#">Add Categories</a></p>
 @endsection
